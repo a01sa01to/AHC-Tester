bbb1fc4ac82c2c78f1bcfd1860f32cfd765915c5
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-08-17 10:31:35 +0900

    Update avg.py: ファイルがなかった時の処理

--- scripts/avg.py
+++ scripts/avg.py
@@ -1,24 +1,27 @@
 import os
 
-FILES = 300
+FILES = 250
 s = 0
 minimum = [0, 1e100]
 maximum = [0, 0]
 
 for i in range(1, FILES + 1):
-    with open(".{}tools{}res{}{}.txt".format(os.sep, os.sep, os.sep, i)) as f:
-        lines = f.readlines()
-        for line in lines:
-            if(line.count("Score =")):
-                t = int(line.split(" = ")[1])
-                if(t):
-                    s += t
-                    if(t < minimum[1]):
-                        minimum = [i, t]
-                    if(t > maximum[1]):
-                        maximum = [i, t]
-                else:
-                    print("Error on {}".format(i))
+    if os.path.exists(".{}tools{}res{}{}.txt".format(os.sep, os.sep, os.sep, i)):
+        with open(".{}tools{}res{}{}.txt".format(os.sep, os.sep, os.sep, i)) as f:
+            lines = f.readlines()
+            for line in lines:
+                if(line.count("Score =")):
+                    t = int(line.split(" = ")[1])
+                    if(t):
+                        s += t
+                        if(t < minimum[1]):
+                            minimum = [i, t]
+                        if(t > maximum[1]):
+                            maximum = [i, t]
+                    else:
+                        print("Error on {}".format(i))
+    else:
+        print("File {} not found".format(i))
 
 
 print("Average score: {}".format(s/FILES))
