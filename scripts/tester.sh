24c1a3d99dab63ea3ebeb12f6714601ac518cb47
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-08-17 10:56:52 +0900

    Update: stderrも確認できるようにする

--- scripts/tester.sh
+++ scripts/tester.sh
@@ -1,3 +1,14 @@
 # Write test script
 # Runs in tools directory
-../a.out < in/$1.txt > out/$1.txt
+../a.out < in/$1.txt 1> out/$1.txt 2> err/$1.txt
+EXCODE=$?
+if [ $EXCODE -gt 0 ]; then
+  cat err/$1.txt
+  exit $EXCODE
+fi
+echo "::group::stdout"
+cat out/$1.txt
+echo "::endgroup::"
+echo "::group::stderr"
+cat err/$1.txt
+echo "::endgroup::"
